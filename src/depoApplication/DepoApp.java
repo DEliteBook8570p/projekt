407a28d91e4a6a125d58db7f5d54d9d540708652
endriti95 <endriti95@users.noreply.github.com>
2017-08-11 15:07:48 +0200

    Artikujt

--- src/depoApplication/DepoApp.java
+++ src/depoApplication/DepoApp.java
@@ -2,17 +2,12 @@ package depoApplication;
 
 import BO.Furnitori;
 import BO.Konsumatori;
+import UI.JFormaKryesore;
 
 public class DepoApp {
-    public static void main(String arg []){
-        try{
-            Konsumatori k=new Konsumatori(5,"Jerry","Hinton","Feugiat Non Lobortis Associates","2706 Urna. Av.","Châteauroux","Bahamas","Centre","9734","0383 628 6280","Endriti95@gmail.com",2);
-           BL.BLKonsumatori.insert(k);
-            //Furnitori f=new Furnitori(10,"Beni Dona", "Breg Dillit", "Prishtina", "Rrafshi Kosoves","Kosove", "10000", "044-152-251", "endritti95@gmail.com",1122);
-            //BL.BLFurnitori.insert(f);
-           
-        } catch (Exception ex) {
-            System.out.println("Exception"+ex);
-        }
+
+    public static void main(String arg[]) {
+        JFormaKryesore f = new JFormaKryesore();
+        f.setVisible(true);
     }
 }
